68e93946b3723fc50658d03f4cb947bfb930c946
Gijsser <gijsser12@gmail.com>
2019-02-22 16:16:39 +0100

    1e versie

--- Wiskunde Week 1/Program.cs
+++ Wiskunde Week 1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace Wiskunde_Week_1
 {
@@ -10,6 +12,65 @@ namespace Wiskunde_Week_1
     {
         static void Main(string[] args)
         {
+            int input;
+
+            input = Ui();
+            Stopwatch sw = Stopwatch.StartNew();
+            FindSmallPrimes(input);
+            sw.Stop();
+            Console.WriteLine("Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+
+        static int Ui()
+        {
+            int input = 0;
+
+            Console.WriteLine("Welcome to the prime number finder");
+            Console.WriteLine("How high would you like to seach?");
+
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("invalid input, try again");
+                }
+            }
+            while (input <= 1);
+
+            return input;
+        }
+
+        static void FindSmallPrimes(int length)
+        {
+            bool[] data = new bool[length + 1];
+            List<int> getallen = new List<int>();
+            int count = 2;
+
+            for (int i = 2; (i * i) < data.Length; i++)
+            {
+                if (!data[i])
+                {
+                    for (int p = 2; p * i < data.Length; p++)
+                    {
+                        if (!data[p * i])
+                        {
+                            data[p * i] = true;
+                            getallen.Add(p * i);
+                            count++;
+                        }
+                    }
+                }
+            }
+            foreach(int i in getallen)
+            {
+                Console.Write(i + ", ");
+            }
+            count = data.Length - count;
+            Console.WriteLine("total primes: {0}", count);
+
         }
     }
 }
